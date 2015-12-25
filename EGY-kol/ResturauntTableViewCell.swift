21af57fd1516c891d076fe0ba190fb00821568e1
Abdellatif Abdelfattah <tifa3333@gmail.com>
2015-12-25 10:15:27 -0800

    Defined the variables for the custom cell

---
+++ EGY-kol/ResturauntTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ResturauntTableViewCell.swift
+//  akli
+//
+//  Created by Abdellatif Abdelfattah on 12/25/15.
+//  Copyright © 2015 Abdellatif Abdelfattah. All rights reserved.
+//
+
+import UIKit
+
+class ResturauntTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
