d0a3f8b24fa8b16411aeddda76224078466cab34
Abdellatif Abdelfattah <tifa3333@gmail.com>
2016-01-13 12:35:27 -0800

    Added animations to the rating screen

--- EGY-kol/ReviewViewController.swift
+++ EGY-kol/ReviewViewController.swift
@@ -6,8 +6,43 @@
 //  Copyright © 2016 Abdellatif Abdelfattah. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class ReviewViewController: UITableViewController {
+class ReviewViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView:UIImageView!
+    @IBOutlet var ratingStackView:UIStackView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        ratingStackView.transform = CGAffineTransformMakeScale(0.0, 0.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+            UIView.animateWithDuration(0.2, delay: 0.0, options: [], animations: {
+            self.ratingStackView.transform = CGAffineTransformIdentity
+            }, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
